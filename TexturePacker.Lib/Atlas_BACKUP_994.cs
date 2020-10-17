039343a48efbeeb32ab3d509494c7cd456e63d1e
z3nth10n <z3nth10n@gmail.com>
2020-10-17 03:51:03 +0200

    remove merge markers

---
+++ TexturePacker.Lib/Atlas_BACKUP_994.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+<<<<<<< HEAD
+namespace _TexturePacker.Lib
+=======
+#if UNITY
+namespace UnityTexturePacker.Lib
+#else
+namespace TexturePacker.Lib
+#endif
+>>>>>>> fe36a5433a51448163b2a3fc34d51a818e040e53
+{
+    /// <summary>
+    /// The texture atlas
+    /// </summary>
+    [Serializable]
+    public sealed class Atlas
+    {
+        /// <summary>
+        /// Width in pixels
+        /// </summary>
+        public int Width;
+
+        /// <summary>
+        /// Height in Pixel
+        /// </summary>
+        public int Height;
+
+        /// <summary>
+        /// List of the nodes in the Atlas. This will represent all the textures that are packed into it and all the remaining free space
+        /// </summary>
+        public List<Node> Nodes;
+    }
+}
